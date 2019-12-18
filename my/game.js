b598ea5f11eeaa8726fcec89fa2f62a15531daf7
Everton Fragoso <everton.lfragoso@gmail.com>
2019-12-18 17:36:46 +1300

    Started my game

---
+++ my/game.js
@@ -0,0 +1,4 @@
+import './game_namespace.js';
+import Init from './game_init.js';
+
+Init();
---
+++ my/game_init.js
@@ -0,0 +1,5 @@
+export default (function Init() {
+  const GAME_CANVAS_SELECTOR = '#game';
+
+  window.Game.gameCanvas = document.querySelector(GAME_CANVAS_SELECTOR);
+});
