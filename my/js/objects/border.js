754753369591614297a5421cb80c71f6d38e8c2e
Everton Fragoso <everton.lfragoso@gmail.com>
2020-01-05 19:06:21 +1300

    Game Border

--- my/js/objects/border.js
+++ my/js/objects/border.js
@@ -1,6 +1,31 @@
-export default function Border() {
-  const b = this;
+export default function Border () {
+  const b = this
+  const G = window.Game
+  const Colours = G.Colours
+  const canvas = G.Canvas
+  const context = G.Context
 
-  b.draw = function() {
-  };
+  let margin = 4
+
+  b.draw = function () {
+    context.beginPath()
+    context.lineWidth = 2
+    context.strokeStyle = Colours.White
+
+    // top border
+    context.moveTo(margin, margin)
+    context.lineTo(canvas.width - margin, margin)
+
+    // left border
+    context.moveTo(margin, margin)
+    context.lineTo(margin, canvas.height)
+
+    // right border
+    context.moveTo(canvas.width - margin, margin)
+    context.lineTo(canvas.width - margin, canvas.height)
+
+    context.stroke()
+    context.closePath()
+    context.lineWidth = 1
+  }
 }
